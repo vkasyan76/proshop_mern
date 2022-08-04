a00c62aca1f359a19ffc6f38981f91121e7e8d3b
vkasyan76 <vkasyan@gmx.de>
2022-08-05 01:32:34 +0200

    Custom Error Handling

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,26 @@ import colors from 'colors'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
 connectDB()
-
 const app = express()
+    // Middleware example:
+    // app.use((req, res, next) => {
+    //     // console.log('HELLO')
+    //     console.log(req.originalUrl)
+    //     next()
+    // })
 
 app.get('/', (req, res) => {
-    res.send('API is running...')
-})
-
-// These are copied to productRoutes and modified:
-// app.get('/api/products', (req, res) => {
-//     res.json(products)
-// })
+        res.send('API is running...')
+    })
+    // These are copied to productRoutes and modified:
+    // app.get('/api/products', (req, res) => {
+    //     res.json(products)
+    // })
 
 // app.get('/api/products/:id', (req, res) => {
 //     const product = products.find((p) => p._id === req.params.id)
@@ -26,6 +31,11 @@ app.get('/', (req, res) => {
 // })
 
 app.use('/api/products', productRoutes)
+    // Middleware when not found:
+app.use(notFound)
+
+// Middleware to overwrite the error:
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 const environment = process.env.NODE_ENV
