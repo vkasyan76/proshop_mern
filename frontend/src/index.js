d152c59b17c515e74af57f6719661de56c0571bf
vkasyan76 <vkasyan@gmx.de>
2022-08-05 18:36:38 +0200

    Create a Redux Store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './store'
 import './bootstrap.min.css'
 import './bootstrap_brad.min.css'
 import './index.css'
@@ -9,7 +11,9 @@ import reportWebVitals from './reportWebVitals'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
 
