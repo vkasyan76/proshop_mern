9a1031b3a130595e9ae9299136b93d28a3a1fd6f
vkasyan76 <vkasyan@gmx.de>
2022-08-08 01:15:32 +0200

    Cart Reducer & Get to Cart Action

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -12,7 +12,7 @@ export const Loader = () => {
         display: 'block',
       }}
     >
-      <span class="sr-only">Loading...</span>
+      <span className="sr-only">Loading...</span>
     </Spinner>
   )
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,12 +3,24 @@ import {
     productListReducer,
     productDetailsReducer,
 } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+    JSON.parse(localStorage.getItem('cartItems')) :
+    []
+
+const initialState = {
+    // cart: { cartItems: 'hello' },
+    cart: { cartItems: cartItemsFromStorage },
+}
 
 const store = configureStore({
     reducer: {
         productList: productListReducer,
         productDetails: productDetailsReducer,
+        cart: cartReducer,
     },
+    preloadedState: initialState,
 })
 
 export default store
