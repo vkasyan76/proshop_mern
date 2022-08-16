8e6e7808494adb66830d511ef3865d1c705b554f
vkasyan76 <vkasyan@gmx.de>
2022-08-16 22:47:33 +0200

    Order Controller & Route

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
-
+import orderRoutes from './routes/orderRoutes.js'
 dotenv.config()
 
 connectDB()
@@ -31,9 +31,9 @@ app.get('/', (req, res) => {
 //     const product = products.find((p) => p._id === req.params.id)
 //     res.json(product)
 // })
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
     // Middleware when not found:
 app.use(notFound)
 
