ed03c384ca369f2003cc21325dbcde02949ce319
vkasyan76 <vkasyan@gmx.de>
2022-08-27 00:13:02 +0200

    Update User Functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,6 +12,7 @@ import {
     userUpdateProfileReducer,
     userListReducer,
     userDeleteReducer,
+    userUpdateReducer,
 } from './reducers/userReducers'
 import {
     orderCreateReducer,
@@ -48,6 +49,7 @@ const store = configureStore({
         userUpdateProfile: userUpdateProfileReducer,
         userList: userListReducer,
         userDelete: userDeleteReducer,
+        userUpdate: userUpdateReducer,
         orderCreate: orderCreateReducer,
         orderDetails: orderDetailsReducer,
         orderPay: orderPayReducer,
